8e0dbef1695a83341b2ef44a8250a8b934d09c68
letys <kontakt@letys.pl>
2015-01-23 23:38:35 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Simple.Data;
+using System;
+
+namespace Letys.SimpleDataExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = Database.OpenNamedConnection("MyConnectionSring");
+
+            db.Stars.Insert(Name: "Sun", Hydrogen: 73.46);
+            db.Stars.Insert(Name: "Alpha Centauri", Hydrogen: 79.68);
+
+            var insertedStars = db.Stars.All();
+
+            foreach (var item in insertedStars)
+            {
+                Console.WriteLine(string.Format("{0}: {1}%", item.Name, item.Hydrogen));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
